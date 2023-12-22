0bdcecc5ed3480f0ba3a87ce6fbe7315afcaaa52
DongwookKim0823 <dwkim0823@naver.com>
2023-12-23 01:58:16 +0900

    리액트보다 100배 쉬운 Vue 반복문 v-for

--- project1/src/App.vue
+++ project1/src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+
+  <div class="menu">
+    <a v-for="a in 메뉴들" :key="a"> {{ a }} </a> <!-- 반복문 형식: <태그 v-for="작명 in 횟수/데이터(array, object) :key="작명"> -->
+  </div>
+
   <div>
-    <h4>xx 원룸</h4>
+    <h4>{{products[0]}}</h4>
     <p>50 만원</p>
   </div>
   <div>
-    <h4>xx 원룸</h4>
-    <p>70 만원</p>
+    <h4>{{products[1]}}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>50 만원</p>
   </div>
 </template>
 
@@ -16,6 +24,7 @@ export default {
   name: 'App',
   data(){ // 데이터 보관하는 곳. 일반 변수 뿐만 아니라 HTML 속성도 데이터 바인딩 가능
     return {
+      메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
@@ -31,6 +40,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
 </style>
