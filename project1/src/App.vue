b65c62a078a5c20121c408f6e868b591bd52f70b
DongwookKim0823 <dwkim0823@naver.com>
2023-12-23 13:29:44 +0900

    Vue 이벤트 핸들러로 click 감지하기 (허위매물 신고버튼 만들기)

--- project1/src/App.vue
+++ project1/src/App.vue
@@ -1,20 +1,29 @@
 <template>
 
   <div class="menu">
-    <a v-for="a in 메뉴들" :key="a"> {{ a }} </a> <!-- 반복문 형식: <태그 v-for="작명 in 횟수/데이터(array, object) :key="작명"> -->
+    <a v-for="a in 메뉴들" :key="a">{{ a }}</a> <!-- 반복문 형식: <태그 v-for="작명 in 횟수/데이터(array, object) :key="작명"> -->
   </div>
+
+  <!--
+  아래와 같은 방법을 반복문으로 해결
+  <div v-for="(a,i) in products" :key="i"> a에는 아이템, i에는 인덱스
+    <h4>{{a}}</h4>
+    <p>50만원</p>
+  </div>
+  -->
 
   <div>
     <h4>{{products[0]}}</h4>
     <p>50 만원</p>
+    <button @click="increase">허위매물신고</button> <span>신고수: {{신고수}}</span> <!-- @click == v-on:click -->
   </div>
   <div>
     <h4>{{products[1]}}</h4>
-    <p>50 만원</p>
+    <p>60 만원</p>
   </div>
   <div>
     <h4>{{products[2]}}</h4>
-    <p>50 만원</p>
+    <p>70 만원</p>
   </div>
 </template>
 
@@ -24,8 +33,14 @@ export default {
   name: 'App',
   data(){ // 데이터 보관하는 곳. 일반 변수 뿐만 아니라 HTML 속성도 데이터 바인딩 가능
     return {
+      신고수 : 0,
       메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  methods: { // 자바스크립트 함수는 여기 위치에 작성해야 함.
+    increase() {
+      this.신고수 += 1; // 내부의 변수를 사용할 때는 this 키워드 잊지 말자
     }
   },
   components: {
