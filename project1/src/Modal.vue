5500337685183ee147289f3d0088cd9d686583e4
DongwookKim0823 <dwkim0823@naver.com>
2023-12-27 00:38:20 +0900

    부모가 가진 데이터를 자식이쓰고 싶으면 props로 전해주셈

--- project1/src/Modal.vue
+++ project1/src/Modal.vue
@@ -2,10 +2,10 @@
   <div class="black-bg" v-if="모달창열렸니 == true">
     <div class="white-bg">
       <img :src="원룸들[누른거].image" style="width:100%">
-      <h4>{{ 원룸들[누른거].title }}</h4>
+      <h4>{{ 원룸들[누른거].title }}</h4> <!-- 참고로 데이터 바인딩은 아래에 있는 데이터만 사용 가능 -->
       <p>{{ 원룸들[누른거].content }}</p>
       <p>{{ 원룸들[누른거].price }} 원</p>
-      <botton @click="모달창열렸니 = false">닫기</botton>
+      <!-- <botton @click="모달창열렸니 = false">닫기</botton> -->
     </div>
   </div>
 </template>
@@ -13,6 +13,11 @@
 <script>
 export default {
     name : 'Modal',
+    props : { // props는 read-only여서, 받아온 거 수정하면 큰일남
+      원룸들 : Array,
+      누른거 : Number,
+      모달창열렸니 : Boolean,
+    }
 }
 </script>
 
