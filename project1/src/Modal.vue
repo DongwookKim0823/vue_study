60214dbd5225939fb3def26c9f865a8d455a1218
DongwookKim0823 <dwkim0823@naver.com>
2023-12-30 02:23:55 +0900

    사용자의 input을 받는 법(v-model)

--- project1/src/Modal.vue
+++ project1/src/Modal.vue
@@ -4,8 +4,10 @@
       <img :src="원룸들[누른거].image" style="width:100%">
       <h4>{{ 원룸들[누른거].title }}</h4> <!-- 참고로 데이터 바인딩은 아래에 있는 데이터만 사용 가능 -->
       <p>{{ 원룸들[누른거].content }}</p>
-      <p>{{ 원룸들[누른거].price }} 원</p>
-      <!-- <botton @click="모달창열렸니 = false">닫기</botton> -->
+      <!-- <input @input="month = $event.target.value"> @input: 입력이 들어올 때 마다 실행, @change: 입력 후 커서를 다른데 찍으면 실행, $event.target.value는 input에 입력한 값임(js 문법) -->
+      <input v-model.number="month"> <!-- 바로 위와 같은 기능. 아래 선언한 month 변수에 값을 저장. .number는 선택 사항이지만 저장할 데이터 타입 명시 효과 -->
+      <p> {{ month }}개월 선택함 : {{ 원룸들[누른거].price * month}} 원</p>
+      <botton @click="$emit('closeMoal')">닫기</botton>
     </div>
   </div>
 </template>
@@ -13,6 +15,11 @@
 <script>
 export default {
     name : 'Modal',
+    data() {
+      return {
+        month : 1, // 문자 입력 받을거면 초기값을 문자로 하셈
+      }
+    },
     props : { // props는 read-only여서, 받아온 거 수정하면 큰일남
       원룸들 : Array,
       누른거 : Number,
